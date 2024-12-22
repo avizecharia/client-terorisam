9d2dea9acdffa7893b650bd4af81318908a293ba
aviel <aviel469@gmail.com>
2024-12-22 17:05:07 +0200

    create the queary for select

---
+++ src/IQuery.ts
@@ -0,0 +1,16 @@
+import {IQuery} from './types/interfaces'
+
+export const query1:IQuery = {sentence:'The most deadly types of attacks',value:1}
+export const query2:IQuery = {sentence:'Areas with the highest casualty rate per incident',value:2}
+export const query3:IQuery = {sentence:'Areas with the highest casualty rate per incident (by area)',value:2.1}
+export const query4:IQuery = {sentence:'Areas with the highest casualty rate per incident (by country)',value:2.2}
+export const query5:IQuery = {sentence:'Areas with the highest casualty rate per incident (by city)',value:2.3}
+export const query6:IQuery = {sentence:'Annual and monthly trends in the frequency of incidents (by year)',value:3}
+export const query7:IQuery = {sentence:'Annual and monthly trends in the frequency of incidents (by years range)',value:3.1}
+export const query8:IQuery = {sentence:'Annual and monthly trends in the frequency of incidents (by last 5 years)',value:3.2}
+export const query9:IQuery = {sentence:'Annual and monthly trends in the frequency of incidents (by last 10 years)',value:3.3}
+export const query10:IQuery = {sentence:'The five most prominent terrorist organizations in a certain region (5 orgaizations)',value:4}
+export const query11:IQuery = {sentence:'The five most prominent terrorist organizations in a certain region (all orgaizations)',value:4.1}
+export const query12:IQuery = {sentence:'Organizations that operated in a certain year with total incidents (by year)',value:5}
+export const query13:IQuery = {sentence:'Organizations that operated in a certain year with total incidents (by organization name)',value:5.1}
+export const query14:IQuery = {sentence:'Areas where certain organizations have carried out the deadliest attacks (by organization name)',value:6}
