fc8b5d0fddc3070be9ec790af7831b8818d759e2
aviel <aviel469@gmail.com>
2024-12-22 17:05:30 +0200

    add types of all json

---
+++ src/types/interfaces.ts
@@ -0,0 +1,51 @@
+ 
+export interface ISecDAta{
+    region:string,
+    numCasualties: number,
+    country: string,
+    city: string,
+    locationArr: ILocation[]
+}
+export interface ILocation {
+    lat: number,
+    lon: number,
+}
+export interface IQuery{
+        sentence:string
+        value:number
+}
+
+
+export  interface IPropsForMarkers {
+    lat?: number
+    lon?: number
+    attackType?:string
+    numCasualties?:number
+    region?:string
+    country?:string
+    city?:string
+    locationArr?:{lat:number,lon:number}[]
+    year?:number
+    month?:number
+    numEvent?:number
+    organizeTopFive?:{region:string,organName:string,numEvent:number}[]
+    organizationName?:string
+    organName?:string
+    eventid?:number
+    iyear?:number
+    imonth?:number
+    iday?:number
+    country_txt?:string
+    region_txt?:string
+    latitude?:number
+    longitude?:number
+    attacktype1_txt?:string
+    targtype1_txt?:string
+    target1?:string
+    gname?:string
+    weaptype1_txt?:string
+    nkill?:number
+    nwound?:number
+    nperps?:number
+    summary?:string
+}
