4aa39bb6d7fcdcb0c514f60185b72b533cd34c32
aviel <aviel469@gmail.com>
2024-12-23 09:15:18 +0200

    add 2.1 query

--- src/componnets/Map.tsx
+++ src/componnets/Map.tsx
@@ -12,86 +12,35 @@ interface Props {
   setQueries: Dispatch<SetStateAction<IQuery[]>>
 }
 export default function Map({ filter, setFilter, queries, setQueries,markers,setMarkers }: Props) {
-    const [a, seta] = useState(false)
+    const [querNum, setQuerNum] = useState(0)
  useEffect(()=>{console.log(filter,markers);
-    filter == 2 && seta(true)
+     markers != undefined && setQuerNum(filter)
  },[markers])
-  const get =   () =>{
+  const get =  () =>{
     console.log(markers ," ma" , filter)
     return true
   }
-
   return (
+    <>
+    { markers != undefined &&
     <div style={{height:'80vh', borderRightColor:'red'}}>
-        {!a && <h1>vvvv</h1>}
-        {a  ? 
-        <div> 
-        <MapContainer
-         center={[markers![0].locationArr![0].lat,
-           markers![0].locationArr![0].lon,]}
-           zoom={10}
-           scrollWheelZoom={false}
-           style={{ height: "80vh" }}
-           >
-         <TileLayer
-           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-           />
-
-        {markers.map(x => <>
-           <Marker
-           position={[
-             x.locationArr![0].lat,
-             x.locationArr![0].lon,
-             
-           ]}
-           >
-           <Popup>
-             region:{x.region}
-             <br></br>
-             city:{x.city}
-             country:{x.country}
-             numCasualties:{x.numCasualties}
-           </Popup>
-         </Marker>
-        </>)} 
-       </MapContainer> 
+        {querNum == 2 && markers[0] != undefined ? <div> 
+        <MapContainer center={[markers![0].locationArr![0].lat,markers![0].locationArr![0].lon,]}zoom={10}scrollWheelZoom={false}style={{ height: "80vh" }}>
+         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+           {markers.map(x => x.locationArr![0].lat != null  &&  x.locationArr![0].lon != null   && <>
+           <Marker position={[ x.locationArr![0].lat, x.locationArr![0].lon, ]}><Popup> region:{x.region} <br></br> city:{x.city}  <br></br>country:{x.country}  <br></br> numCasualties:{x.numCasualties}</Popup></Marker></> )} 
+         </MapContainer> 
         </div> :
-         <h1>waittt</h1>}
-        {/* { a && markers[0] != undefined && filter === 2  &&
-         <> 
-         <MapContainer
-          center={[markers![0].locationArr![0].lat,
-            markers![0].locationArr![0].lon,]}
-            zoom={10}
-            scrollWheelZoom={false}
-            style={{ height: "80vh" }}
-            >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-
-         {markers.map(x => <>
-            <Marker
-            position={[
-              x.locationArr![0].lat,
-              x.locationArr![0].lon,
-              
-            ]}
-            >
-            <Popup>
-              region:{x.region}
-              <br></br>
-              city:{x.city}
-              country:{x.country}
-              numCasualties:{x.numCasualties}
-            </Popup>
-          </Marker>
-         </>)} 
-        </MapContainer> 
-         </>
-         }  */}
+      querNum == 2.1 && markers[0].locationArr != undefined? <div> 
+      <MapContainer center={[markers![0].locationArr![0].lat,markers![0].locationArr![0].lon,]}zoom={10}scrollWheelZoom={false}style={{ height: "80vh" }}>
+       <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+         {markers.map(x => x.locationArr![0].lat != null  &&  x.locationArr![0].lon != null   && <>
+         <Marker position={[ x.locationArr![0].lat, x.locationArr![0].lon, ]}><Popup> region:{x.region} <br></br> city:{x.city}  <br></br>country:{x.country}  <br></br> numCasualties:{x.numCasualties}</Popup></Marker></> )} 
+       </MapContainer> 
+      </div>
+         : <h1>waittt</h1>} 
     </div>
+}
+    </>
   )
 }
