36613f224b4fcd5e0b62138df69b362deb7f4749
aviel <aviel469@gmail.com>
2024-12-23 17:43:48 +0200

    add props and call to grafh

--- src/componnets/Nav.tsx
+++ src/componnets/Nav.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { IPropsForMarkers, IQuery } from "../types/interfaces";
 import Select from "./Select";
+import Graph from "./Grafh";
 interface Props {
   markers: IPropsForMarkers[];
   setMarkers: (n: IPropsForMarkers[]) => void;
@@ -8,8 +9,12 @@ interface Props {
   setFilter: (num: number) => void;
   queries: IQuery[];
   setQueries: Dispatch<SetStateAction<IQuery[]>>
+  first: IPropsForMarkers[];
+  setfirst: (n: IPropsForMarkers[]) => void;
+  third: IPropsForMarkers[];
+  setThird: (n: IPropsForMarkers[]) => void;
 }
-export default function Nav({ filter, setFilter, queries, setQueries,markers,setMarkers }: Props) {
+export default function Nav({ filter, setFilter, queries, setQueries,markers,setMarkers,first,setfirst,setThird,third }: Props) {
   return (
     <div>
       Nav
@@ -21,6 +26,12 @@ export default function Nav({ filter, setFilter, queries, setQueries,markers,set
         queries={queries}
         setQueries={setQueries}
       />
+      {filter == 1 ?
+        <Graph bars={[{ key: "numCasualties", color: "#8894d8", name: "num of casualties" }]} data={first!} xKey={'attackType'} /> : ""}
+      {filter == 3 ?
+        <Graph bars={[{ key: "numEvent", color: "#7344d8", name: "num of events" }]} data={third!} xKey={'month'} /> : ""}
+      {filter == 3.1 ?
+        <Graph bars={[{ key: "numEvent", color: "#2344d8", name: "num of events" }]} data={third!} xKey={'year'} /> : ""}
     </div>
   );
 }
